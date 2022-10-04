cab233b95eb3d86a3927ed16f31a0601b2ac0a40
stannyu <stas.yudzitski@yandex.ru>
2022-10-04 13:42:10 +0200

    Initial Commit

---
+++ CoffeMasters/CoffeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeMastersApp.swift
+//  CoffeMasters
+//
+//  Created by Stas Yudzitski on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
